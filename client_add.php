856f49e143a3ed4340f9a391b85d1484e9addae8
Ravi Thanki <ravithanki15@yahoo.com>
2016-07-13 18:42:39 +0530

    Add new method

--- client_add.php
+++ client_add.php
@@ -11,9 +11,30 @@ $data['surname'] = 'Test';
 $data['company'] = 'Test';
 $data['email'] = 'Test';
 
+
+/* for add client */
 $res = $obj->post('client/add',$data);
-//$res = $obj->post('client/list',$data);
-//$res = $obj->post('client/367650/get');
+
+/* for list of all client */
+$data['email'] = 'Test';
+$res = $obj->post('client/list',$data);
+
+/* for get details of client by id */
+$res = $obj->post('client/367650/get');
+
+/* for generate invoice */
+
+$invoice['description'] = array('Test Description');
+$invoice['unit_cost'] = array(10);
+$invoice['quantity'] = array(1);
+$res = $obj->post('client/367650/add_invoice');
+
+
+/* for cancel invoice */
+$res = $obj->post('invoice/invoice_id/cancel');
+
+invoice_id : when you create invoice snapbill send invoice id in response on your set callback url
+
 print_r($res);
 exit;
 
